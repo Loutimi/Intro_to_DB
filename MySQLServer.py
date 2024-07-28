9c43524e96b1496df2a9f3a77cdc58a71a8b742e
Loutimi <Loutimi59@gmail.com>
2024-07-28 14:48:14 +0100

    Modify MySQLServer.py to include exception handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,13 +1,22 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Denomdemon12.",
-    database="alx_book_store"
-)
+# FOR EXCEPTION HANDLING
+try:
+        
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Denomdemon12.",
+        database = 'alx_book_store',
+    )
+    if mydb.is_connected():
+        # TO CHECK IF THE DATABASE CONNECTS
+        print("Database 'alx_book_store' created successfully!")
+    
+    else:
+        # USING EXECUTE FUNCTION TO CREATE DATABASE IF IT DOESN'T EXIST
+        cursor = mydb.cursor()
+        cursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
 
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-# for i in mycursor:
-#     print(i)
+except mysql.connector.Error as er:
+    print("Something went wrong: {}".format(er))
